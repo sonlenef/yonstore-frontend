8a07bd1e3df1133a63a38f784a5c74e8b175793c
Le Son <leson@Les-MacBook-Pro.local>
2020-06-04 22:43:23 +0700

    04/06

--- src/js/menu/Menu.jsx
+++ src/js/menu/Menu.jsx
@@ -22,20 +22,31 @@ class Menu extends Component {
             <div ref={this.menuButtonRef} className="menu">
                 <div className="menu-button" onClick={() => this.hanldClick(true)}>
                     <ul className="yon">
-                    <li><span>Y</span></li>
-                    <li><span>o</span></li>
-                    <li><span>n</span></li>
+                        <li><span>Y</span></li>
+                        <li><span>o</span></li>
+                        <li><span>n</span></li>
                     </ul>
                     <ul className="store">
-                    <li><span>S</span></li>
-                    <li><span>t</span></li>
-                    <li><span>o</span></li>
-                    <li><span>r</span></li>
-                    <li><span>e</span></li>
+                        <li><span>S</span></li>
+                        <li><span>t</span></li>
+                        <li><span>o</span></li>
+                        <li><span>r</span></li>
+                        <li><span>e</span></li>
                     </ul>
                     <img className="menu-icon" src={menuIcon} alt="menu"/>
+                    <ul className="social-title">
+                        <li><span>Social</span></li>
+                        <li><span>Network</span></li>
+                    </ul>
+                    <ul className="social-icon">
+                        <li><div></div></li>
+                        <li><div></div></li>
+                        <li><div></div></li>
+                        <li><div></div></li>
+                    </ul>
                 </div>
                 <div ref={this.menuContentRef} className="menu-content" id="menu-content" onClick={() => this.hanldClick(false)}>
+                    <div className="menu-img-background"></div>
                     <MenuContent/>
                 </div>
             </div>
