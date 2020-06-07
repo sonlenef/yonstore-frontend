019ce26c6b0628c35d2e4c1bc0fafe58a620e636
Le Son <leson@Les-MacBook-Pro.local>
2020-06-07 21:47:29 +0700

    07/06

---
+++ src/js/home/HomeBody.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../css/homehead.css'
+
+class HomeBody extends Component {
+    render() {
+        return (
+
+            <div className="home-body">
+            </div>
+        )
+    }
+}
+
+export default HomeBody;
