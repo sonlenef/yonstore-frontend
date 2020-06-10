52a2271ed56ee108622828b2754f35fb9141045e
Le Son <leson@Les-MacBook-Pro.local>
2020-06-10 15:29:32 +0700

    10/06

--- src/js/home/HomeBody.jsx
+++ src/js/home/HomeBody.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import '../../css/homehead.css'
+import '../../css/homebody.css'
+import SlideBox from './slide-box/SlideBox'
+import SlideBox1 from './slide-box/SlideBox1'
 
 class HomeBody extends Component {
     render() {
         return (
 
             <div className="home-body">
+                <div className="home-body-1"><SlideBox/></div>
+                <div className="home-body-2"><SlideBox1/></div>
             </div>
         )
     }
